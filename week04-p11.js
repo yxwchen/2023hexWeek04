5bcd3f7bb96c2cd3b7b2ae5b810cb226b8816765
claire <claire1212@gmail.com>
2023-12-18 22:49:36 +0800

    11

--- week04-p11.js
+++ week04-p11.js
@@ -93,19 +93,27 @@ function printBmi(height, weight) {
             console.log(`您的體重過輕`);
         } else if (bmi >= 18.5 && bmi < 24) {
             console.log(`您的體重正常`);
-        } else {
-            console.log(`其他`);
+        } else if (bmi >= 24 && bmi < 27) {
+            console.log(`您的體重過重`);
+        }
+        else if (bmi >= 27 && bmi < 30) {
+            console.log(`您的體重輕度肥胖`);
+        } else if (bmi >= 30 && bmi < 35) {
+            console.log(`您的體重中度肥胖`);
+        } else if (bmi >= 35) {
+            console.log(`您的體重重度肥胖`);
         }
 
     }
+
 }
 printBmi("身高", "體重");
 printBmi(178, 20);
 printBmi(178, 70);
 printBmi(178, 85);
-// printBmi(178, 90);
-// printBmi(178, 110);
-// printBmi(178, 130);
+printBmi(178, 90);
+printBmi(178, 110);
+printBmi(178, 130);
 
 
 
